64987e9b98af4566cec448add76a1cdf887c1b9e
peter7788yes <63861260+peter7788yes@users.noreply.github.com>
2020-04-17 14:53:12 +0000

    Create CheckboxItem.cs

---
+++ CheckboxItem.cs
@@ -0,0 +1,49 @@
+using System.Runtime.CompilerServices;
+
+public class CheckboxItem
+{
+	[CompilerGenerated]
+	private string <Text>k__BackingField;
+
+	[CompilerGenerated]
+	private object <Value>k__BackingField;
+
+	public string Text
+	{
+		[CompilerGenerated]
+		get
+		{
+			return <Text>k__BackingField;
+		}
+		[CompilerGenerated]
+		set
+		{
+			<Text>k__BackingField = value;
+		}
+	}
+
+	public object Value
+	{
+		[CompilerGenerated]
+		get
+		{
+			return <Value>k__BackingField;
+		}
+		[CompilerGenerated]
+		set
+		{
+			<Value>k__BackingField = value;
+		}
+	}
+
+	public CheckboxItem(string text, object value)
+	{
+		Text = text;
+		Value = value;
+	}
+
+	public override string ToString()
+	{
+		return Text;
+	}
+}
